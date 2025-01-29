80ddaa530b4eb872d6e292ed3340e96b03b02d83
Sakethganakrishna-medidi <sakethganakrishna@gmail.com>
2025-01-29 10:14:56 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,8 @@
+public class Area {
+    public static void main(String[] args) {
+        int a = 44;
+        float b = 3.0f;
+        float Area = a*b;
+        System.out.println(Area);
+    }
+}
